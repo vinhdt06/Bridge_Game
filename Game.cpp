67c5500cf0745c9b65a9365d9ff91c8ed235d80b
vinhdt06 <24021676@vnu.edu.vn>
2025-03-18 12:38:37 +0700

    Change Game

--- Game.cpp
+++ Game.cpp
@@ -11,10 +11,15 @@ SDL_Texture* homeBackground = nullptr;
 SDL_Texture* levelMenuBackground = nullptr;
 SDL_Texture* gameBackground = nullptr;
 std::vector<Platform> platforms;
+SDL_Rect hero = { 100, SCREEN_HEIGHT - 100, 35, 35 };
+SDL_Rect stick = { 0, 0, STICK_WIDTH, 0 };
 
+double stickAngle = 0;
 int platformsPassed = 0;
 int platformsWin = WIN_PLATFORMS;
 int newLevel = 1;
+int stickTime = 0;
+int heroPos = 0;
 
 void createPlatforms() {
 	platforms.clear();
@@ -33,7 +38,16 @@ void createPlatforms() {
 void Levels(int level) {
 	platformsWin = WIN_PLATFORMS + (level - 1) * 2;
 	createPlatforms();
-
+	hero.x = platforms[0].x + (platforms[0].w - hero.w) / 2;
+	hero.y = platforms[0].y - hero.h;
+	stickAngle = false;
+	stickLength = false;
+	stickTurn = false;
+	heroWalk = false;
+	heroFall = false;
+	stickDown = false;
+	heroPos = hero.x;
+	platformsPassed = 0;
 }
 
 void MainMenu() {
--- Game.h
+++ Game.h
@@ -15,6 +15,31 @@ const int PLATFORM_POS = 330;
 const int PLAT_DIS_MAX = 100;
 const int PLAT_DIS_MIN = 200;
 const int WIN_PLATFORMS = 4;
+const int HERO_SPEED = 6;
+const int TIME_STICK = 100;
+
+extern SDL_Window* window;
+extern SDL_Renderer* renderer;
+extern SDL_Texture* heroCharacter;
+extern SDL_Texture* homeBackground;
+extern SDL_Texture* levelMenuBackground;
+extern SDL_Texture* gameBackground;
+extern std::vector<Platform> platforms;
+extern SDL_Rect hero;
+extern SDL_Rect_stick;
+
+extern bool stickLength;
+extern bool stickTurn;
+extern bool stickDown;
+extern bool heroWalk;
+extern bool heroFall;
+
+extern double stickAngle;
+extern int platformsPassed;
+extern int platformsWin;
+extern int newLevel;
+extern int stickTime;
+extern int heroPos;
 
 enum gameState {
 	MAIN_MENU,
